7ae465dba04c8904e450182b1e132041f1509bfd
Anneloes <anneloes9405@hotmail.com>
2020-02-10 05:26:18 +0000

    made preliminary queries for users, now adding maps

--- routes/users.js
+++ routes/users.js
@@ -5,12 +5,20 @@
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
+ // every /.. stands for /users.., because of the mounting in index.js
+
 const express = require('express');
 const router  = express.Router();
 
 module.exports = (db) => {
-  router.get("/", (req, res) => {
-    db.query(`SELECT * FROM users;`)
+  // gets user
+  // TODO check if req.params is right use
+  router.get("/:id", (req, res) => {
+    console.log("Id: ", req.params)
+    db.query(`
+    SELECT * FROM users
+    WHERE users.id = ${req.params};
+    `)
       .then(data => {
         const users = data.rows;
         res.json({ users });
@@ -21,5 +29,47 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
   });
+
+  //logs user out
+  router.post("/:id/logout", (...))
+
+  // registers user
+  router.post
+
+  router.post("/register/:id", (req, res) => {
+    db.query(`
+    INSERT INTO users (name)
+    VALUES $1
+    `, name)
+    .then(data => {
+      const users = data.rows;
+      res.json({ users });
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
+  })
+
+  router.get("/:id/favourites", (req, res) => {
+    db.query(`
+    SELECT maps.title
+    FROM maps
+    JOIN favourite_maps ON favourite_maps.user_id = maps.viewer_id
+    JOIN users ON favourite_maps.user_id = users.id
+    WHERE users.id = ${req.params}
+    `, )
+  })
+
+  router.get("/:id/owned", (req, res) => {
+    db.query(`
+    SELECT maps.title
+    FROM maps
+    JOIN favourite_maps ON favourite_maps.user_id = maps.owner_id
+    JOIN users ON favourite_maps.user_id = users.id
+    WHERE users.id = ${req.params}
+    `, )
+  })
   return router;
 };
