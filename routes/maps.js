12b32194354e1a603342dfc7cdf63a11fe070b2f
lim1017 <lim1017@hotmail.com>
2020-02-11 02:47:59 +0000

    can now label created points

--- routes/maps.js
+++ routes/maps.js
@@ -22,6 +22,13 @@ module.exports = (db) => {
     })
   })
 
+  router.post("/update", (req, res) => {
+
+    console.log('updating description!!!!!!!!')
+    console.log(req.body)
+
+  })
+
   router.get("/:id", (req, res) => {
     let query = `
     SELECT *
