1918e8cea2a28534f84dbd833640dd078bd982bf
Anneloes <anneloes9405@hotmail.com>
2020-02-13 22:07:03 +0000

    changed button with edit alert to modal

--- public/mapCreate.js
+++ public/mapCreate.js
@@ -9,8 +9,31 @@ let markers_count = 0;
 
 $("document").ready(function() {
 
-  $(".edit-mode").hide()
-  $(".empty-div").hide()
+  // Get the modal
+  var modal = document.getElementById("myModal");
+
+  // Get the button that opens the modal
+  var btn = document.getElementById("edit");
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on the button, open the modal
+  btn.onclick = function() {
+  modal.style.display = "block";
+  }
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+  modal.style.display = "none";
+  }
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
   currentMap = 1;
   mapSetup();
